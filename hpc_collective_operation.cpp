ff7039cdadd74ad68a4bec7a8122540b1816a025
GianiP <giovidj.gp@gmail.com>
2019-11-02 10:53:02 +0100

    me

--- hpc_collective_operation.cpp
+++ hpc_collective_operation.cpp
@@ -9,18 +9,17 @@
 #define SEED 35791246
 
 int main ( int argc , char *argv[ ] ){
-	// variables for mesurng the times 
-	double start_time_proce, end_time_proce, start_time_read, end_time_read,start_time_comm, end_time_comm ;   
-	// variables for info about the processor
-	int myid , numprocs , proc ;
+
+	double start_time_proce, end_time_proce, start_time_read, end_time_read,start_time_comm, end_time_comm ;   	// variables for mesurng the times  
+	int myid , numprocs , proc ;  	// variables for info about the processor
 	//about MPI
 	MPI_Status status;
 	MPI_Request request;
 	// master process
 	int master = 0;
-	unsigned int somma =0;
-	unsigned int somma_parziale =0;
-	unsigned int totsomma=0;
+	int tag = 123;
+	long long int somma =0;
+	long long int somma_parziale=0;
 	
 	//start the parallel part of the program 
 	MPI_Init(&argc,&argv);
@@ -35,61 +34,61 @@ int main ( int argc , char *argv[ ] ){
   
 	start_time_read=MPI_Wtime();  //start to take the tame for read the number in the file
 
-	// inizialize the variable for read the file
-	unsigned int ** vett ;
+/*	
+	long long int ** vett ;// inizialize the variable for read the file
 	FILE *fd; //pointers to file
-	unsigned int numero;
+	long long int numero;
   
-	/* allocation */
-	vett =(unsigned int **) malloc(sizeof(unsigned int));
+	vett =(long long int **) malloc(sizeof(long long int));	// allocation 
   
-	/* open the file */
+	// open the file 
 	fd=fopen("numero.txt", "r"); //open in read mode the file (in this case my file name is "numero") and read the number inside 
   
-	/* error management for file */
+	// error management for file 
 	if( fd==NULL ) {
   		perror("Errore in apertura del file");
   		exit(1);
 	}
 
-	/* read the the number inside the file and put the numver inside the variavle vett */		
+	// read the the number inside the file and put the numver inside the variavle vett 		
 	numero=fscanf(fd, "%i", &vett);  
 
-	/* close the file */
-	fclose(fd);
+	fclose(fd);  	// close the file 
 	end_time_read=MPI_Wtime(); //stop the time for read the file
 	printf ( "\n # walltime on master processor : %10.8f \n", end_time_read - start_time_read ) ; //print the time for read the file
+*/
 
+
+	long long int vett = atoll(argv[1]);
+	end_time_read=MPI_Wtime(); //stop the time for read the file
 	//check if the number inside the file is divisibile by the number of processors  
-	int divisibile = vett % numproc; //take the rest 
+	long long int divisibile = vett % numproc; //take the rest 
 	if (divisibile != 0){  // if the number is not divisibile by number of processors i give the number divisibile and i will
 						   // add the extra numbers to the master processor
 		vett = vett - divisibile;
 	}
-	unsigned int array[(vett/numproc)+divisibile];
-	for (int i = (vett/numproc)*proc; i<(vett/numproc)*(proc+1); i++){
-		array[i] = i;   //i create the array with the number that the processor have to sum
-		somma_parziale = somma_parziale + array[i]; //make the partial sum for that processor
+	long long int array[(vett/numproc)+divisibile];
+	long long int i;
+	for (i = (vett/numproc)*proc; i<(vett/numproc)*(proc+1); i++){
+		array[i] = i;   // create the array with the number that the processor have to sum
+		somma_parziale = somma_parziale + i; //make the partial sum for that processor
 	}
 
 	if (myid ==0) { //if I am the master process gather results from others
-		for (int i = vett; i<vett+divisibile; i++){
-			somma_parziale = somma_parziale + array[i];
+		long long int j;
+		for (j = vett; j<vett+divisibile; j++){
+			somma_parziale = somma_parziale + j;
 		}
 		somma = somma_parziale;
+	}
 		start_time_comm=MPI_Wtime(); 
-    	for (proc=1; proc<numprocs ; proc++) {
-    	    totsomma= totsomma+somma[proc] ;
-		}
-		end_time_comm=MPI_Wtime(); 
+     	MPI_Reduce(&somma_parziale, &somma, 1, MPI_LONG_LONG, MPI_SUM, 0, MPI_COMM_WORLD);
+    	end_time_comm=MPI_Wtime(); 
     	printf("%lli\n", somma);  //master processor print the total sum
     	end_time_proce=MPI_Wtime();   // stop the time that rappresent the time for run the program in the master (all program) because the master makes the last operation
-    	printf ( "\n # walltime on master processor : %10.8f \n", end_time_proce - start_time_proce ) ;
-	}
-  
-	else {   // for all the slave processes send results to the master /
-		MPI_Gather(somma_parziale, 1, MPI_UNSIGNED, somma, 0, MPI_UNSIGNED, 0, MPI_COMM_WORLD)
-		end_time_proce=MPI_Wtime(); //stop the time that rappresent the time for run the program in the processor
-		printf ( "\n # walltime on  processor %i : %10.8f \n",&proc, end_time_proce - start_time_proce ) ;
-	}
+    	printf ( "\n # walltime : %10.8f  \t on the processor: %i\n" , end_time_proce - start_time_proce, myid ) ;
+    	printf ( "\n # time to read the number : %10.8f \t on the processor: %i\n", end_time_read - start_time_read, myid ) ;
+    	printf ( "\n # time for communicate to the master the partial sum : %10.8f \t on the processor: %i\n \n", end_time_comm - start_time_comm, myid ) ;
+    	MPI_Finalize() ;
 }
+  
